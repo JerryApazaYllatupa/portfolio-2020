c47bb6341f6b00847ad4bd6e3e3835c32ea94033
Jerry Apaza <jerryapazayllatupa7@gmail.com>
2020-09-28 15:02:51 -0500

    firebase integrado

---
+++ src/js/app.js
@@ -0,0 +1,50 @@
+import {
+   getId,
+   getClass,
+   getAll,
+   listener,
+   getName,
+   ajax,
+   ruteImg,
+   rutePeticion,
+} from './library/library';
+
+
+
+
+// =============== Section menu  ==============
+let active = getId("icon-hamburger"),
+   menu = getId("menu-principal"),
+   header = getId("header-principal");
+
+
+// Active menu
+listener(active, 'click', () => {
+   header.classList.toggle("active")
+
+})
+
+//  active text
+listener(menu, 'click', (e) => {
+   let a = e.target.parentElement,
+      span = e.target.parentElement.parentElement,
+      li = getAll('menu-item')
+
+
+
+   if (a.nodeName == "LI") {
+      clearItem(li)
+      a.classList.add('active')
+   } else if (span.nodeName == "LI") {
+      clearItem(li)
+      span.classList.add('active')
+   }
+
+})
+
+//  clear item active 
+let clearItem = (li) => {
+   li.forEach(item => {
+      item.classList.remove('active')
+   });
+}
